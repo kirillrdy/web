1cb0b82a94159a70e2117a6fdd1a11b051e41bd1
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2017-05-01 14:37:27 +0900

    Also provide path for reset css

--- css/reset.go
+++ css/reset.go
@@ -6,10 +6,15 @@ import (
 	"runtime"
 )
 
+const resetCSSFilename = "reset.css"
+
+//ResetCSSPath is the path that you should use for reset css file
+const ResetCSSPath = "/assets/" + resetCSSFilename
+
 //ServeResetCSS as promised serves reset css file
 func ServeResetCSS(response http.ResponseWriter, request *http.Request) {
 	_, currentFile, _, _ := runtime.Caller(0)
 	packageDir := path.Dir(currentFile)
 
-	http.ServeFile(response, request, packageDir+"/reset.css")
+	http.ServeFile(response, request, packageDir+"/"+resetCSSFilename)
 }
