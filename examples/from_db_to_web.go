c91378f95f6c2b84aeb42fa244c91ca3f1c44e27
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2021-07-05 16:50:11 +1000

    Moved schema stuff out

--- examples/from_db_to_web.go
+++ examples/from_db_to_web.go
@@ -5,36 +5,37 @@ import (
 	. "github.com/kirillrdy/web/html"
 )
 
+var tables = struct {
+	users db.Table
+	posts db.Table
+}{
+	"users",
+	"posts",
+}
+var users = struct {
+	id   db.Column
+	name db.Column
+}{
+	tables.users.Column("id"),
+	tables.users.Column("name"),
+}
+var posts = struct {
+	title  db.Column
+	userId db.Column
+}{
+	tables.posts.Column("title"),
+	tables.posts.Column("user_id"),
+}
+
 func main() {
-	var tables = struct {
-		users db.Table
-		posts db.Table
-	}{
-		"users",
-		"posts",
-	}
-	var users = struct {
-		id   db.Column
-		name db.Column
-	}{
-		tables.users.Column("id"),
-		tables.users.Column("name"),
-	}
-	var posts = struct {
-		title  db.Column
-		userId db.Column
-	}{
-		tables.posts.Column("title"),
-		tables.posts.Column("user_id"),
-	}
 
 	rows := db.Select(posts.title, users.name).From(tables.posts).Join(tables.users, users.id, posts.userId).Execute()
 	for _, row := range rows {
 		Div(Class("foo"))(
 			P()(
 				Text(row.String(posts.title)),
+				Text(row.String(users.name)),
 			),
 		)
 	}
-
 }
